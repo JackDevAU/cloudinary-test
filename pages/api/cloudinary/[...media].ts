7d59f022f45d8bce12b9a65f34a233fac3a472d5
JackDevAU <jpr178@live.com.au>
2025-02-06 15:16:54 +1000

    updates

--- pages/api/cloudinary/[...media].ts
+++ pages/api/cloudinary/[...media].ts
@@ -11,7 +11,7 @@ export default createMediaHandler({
 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
 	api_key: process.env.CLOUDINARY_API_KEY!,
 	api_secret: process.env.CLOUDINARY_API_SECRET!,
-	authorized: async (req, _res) => {
+	authorized: async (req) => {
 		try {
 			if (process.env.NODE_ENV === "development") {
 				return true;
